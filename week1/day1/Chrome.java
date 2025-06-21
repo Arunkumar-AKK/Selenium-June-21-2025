615d3c55a849a58cbb05d8d6f5a4e3c94b58aa8c
Arunkumar-AKK <arunkumardasan@outlook.com>
2025-06-21 23:42:30 +0530

    Add files via upload
    Week1 Assignment 1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		
+		float browserVersion  = 91.0f;
+		String developerType = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("The Desired Output is" +" \n" +"browserVersion:"+ browserVersion +" \n"+"developerType:" + developerType 
+				+ " \n" + "isBeta:" + isBeta 
+				+ " \n" + "releaseYear:" +releaseYear + " \n" + "shortcutKey:" + shortcutKey + " \n" + 
+				"All the Output printed");
+		
+	}
+
+}
